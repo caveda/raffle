74d1cbcc5a03b70651cdde464dad9bb03443fee1
victorcaveda <caveda@gmail.com>
2016-05-04 23:30:34 +0200

    Basic functional random generator between two numbers.

--- script.js
+++ script.js
@@ -1,69 +1,47 @@
-function getRandomColor() 
+// Global Variables
+var mCurrentWinner;
+var mMinTicketNumber = 25;
+var mMaxTicketNumber = 100;
+
+
+/// ----------------------------------------------------------
+/// Calculate next winner number
+/// ----------------------------------------------------------
+function getNextWinner(min, max) 
 {
- 	 	 var letters = '0123456789ABCDEF'.split('');
- 	 	 var color = '#';
- 	 	 for (var i = 0; i < 6; i++ ) {
- 	 	 color += letters[Math.round(Math.random() * 15)];
- 	 	 }
- 	 	 return color;
+   var nextWinner = Math.floor(Math.random() * (max-min+1)) + min
+   return nextWinner;
 }
 		
 
-var clickedTime; var createdTime; var reactionTime;
-function makeBox() {
-	 	
-	 	 var time=Math.random();
-	 	
-	 	 time=time*5000;
-	 	
-			 setTimeout(function() {
-				
-				 if (Math.random()>0.5) {
-				
-					
-document.getElementById("box").style.borderRadius="100px";
-					
-				 } else {
-					
-					
-document.getElementById("box").style.borderRadius="0";
-					
-					
-				 }
-					
-				 var top=Math.random();
-					
-				 top=top*300;
-					
-				 var left=Math.random();
-					
-				 left=left*500;
-					
-				 document.getElementById("box").style.top=top+"px";
-					
-				 document.getElementById("box").style.left=left+"px";
-				
-				
-document.getElementById("box").style.backgroundColor=getRandomColor();
-			
-				 document.getElementById("box").style.display="block";
-					
-				 createdTime=Date.now();
-			
-			 }, time);
-	 	
+/// ----------------------------------------------------------
+/// Read all raffle data from the HTML components
+/// ----------------------------------------------------------
+function readRaffleDataFromUI () 
+{
+    mMinTicketNumber = parseInt(document.getElementById("first_number").value);
+    mMaxTicketNumber = parseInt(document.getElementById("last_number").value);
 }
-document.getElementById("box").onclick=function() {
-	 	
-	 	 clickedTime=Date.now();
-		 	
-	 	 reactionTime=(clickedTime-createdTime)/1000;
-		 	
-	 	 document.getElementById("time").innerHTML=reactionTime;
-	 	
-	 	 this.style.display="none";
-	 	
-	 	 makeBox();
+
+/// ----------------------------------------------------------
+/// Display the next  all raffle data from the HTML components
+/// ----------------------------------------------------------
+function showWinnerNumber (winnerNumber) 
+{
+    document.getElementById("winner_number").value = winnerNumber;
 }
-	 	
-makeBox();
+
+
+/// ----------------------------------------------------------
+/// Main function
+/// ----------------------------------------------------------
+function startRaffle () {
+    // init
+    readRaffleDataFromUI();
+    
+    // calculate next winner
+    mCurrentWinner = getNextWinner(mMinTicketNumber, mMaxTicketNumber);
+    
+    // show next winner
+    showWinnerNumber(mCurrentWinner);
+}
